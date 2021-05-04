ca6c6163077b8c2c96e3bfff8a93f7b9b561d5b1
masayoshi sato <masayoshi418979@icloud.com>
2021-05-05 00:06:56 +0900

    changed file

--- movieLens.R
+++ movieLens.R
@@ -1,3 +1,4 @@
+#used following libraries
 library(tidyverse)
 library(caret)
 library(data.table)
@@ -75,8 +76,14 @@ edx %>% group_by(rating) %>%
 str(edx)
 summary(edx)
 
-#unique users and movies
-edx %>% summarize(n_users =n_distinct(userId), n_movies= n_distinct(movieId))
+#unique values
+edx %>% summarize(n_users =n_distinct(userId), n_movies= n_distinct(movieId),
+                  n_rating =n_distinct(rating),n_title=n_distinct(title),
+                  n_genres= n_distinct(genres))
+
+#n_users n_movies n_rating n_title n_genres
+#1   69878    10677       10   10676      797
+
 
 #plot
 
@@ -104,6 +111,76 @@ edx %>%
 
 mean(edx$rating)
 
+#test and training data
+set.seed(2021, sample.kind = "Rounding") 
+test_index <- createDataPartition(y = edx$rating, times = 1, p = 0.2, 
+                                  list = FALSE)
+train_set <- edx[-test_index,]
+test_set <- edx[test_index,]
 
+#To make sure we don’t include users and movies in the test set 
+#that do not appear in the training set, 
+#we remove these entries using the semi_join function:
+test_set <- test_set %>% 
+  semi_join(train_set, by = "movieId") %>%
+  semi_join(train_set, by = "userId")
 
+#RMSE function
+RMSE <- function(true_ratings, predicted_ratings){
+  sqrt(mean((true_ratings - predicted_ratings)^2))
+}
+
+#1 Naive model assuming the same rating for all movies. 
+mu_hat <- mean(train_set$rating)
+naive_rmse <- RMSE(test_set$rating, mu_hat)
+naive_rmse
+#>[1] 1.059586
+
+#2 movie effects
+mu <- mean(train_set$rating)
+
+movie_avg <- train_set %>%
+  group_by(movieId)%>%
+  summarize(b_i =mean(rating -mu))
+
+movie_effect_pred <- mu + test_set %>%
+  left_join(movie_avg, by="movieId") %>%
+  pull(b_i)
+
+movie_effect_rmse <- RMSE(test_set$rating, movie_effect_pred)
+movie_effect_rmse
+#[1] 0.9434034
+
+#3 user effects
+
+user_avg <- train_set %>%
+  left_join(movie_avg, by= "movieId") %>%
+  group_by(userId) %>%
+  summarize(b_u = mean(rating- mu -b_i))
   
+movie_user_effect_pred <- test_set %>%
+  left_join(movie_avg, by="movieId") %>%
+  left_join(user_avg, by="userId") %>%
+  mutate(pred = mu + b_i + b_u)%>%
+  pull(pred)
+
+movie_user_effect_rmse <- RMSE(test_set$rating, movie_user_effect_pred)
+movie_user_effect_rmse
+#[1] 0.8660241
+
+
+# dealing with validation file
+
+valid_pred_rating <- validation %>%
+  left_join(movie_avg, by="movieId") %>%
+  left_join(user_avg, by="userId") %>%
+  mutate(pred = mu + b_i + b_u)%>%
+  pull(pred)
+
+#replace NA by mu
+sum(is.na(valid_pred_rating))
+#there is 11 NAs in the valid_pred_rating. Thus replace them with mean rating
+valid_pred_rating <- replace(valid_pred_rating, which(is.na(valid_pred_rating)), mu)
+
+RMSE(validation$rating,valid_pred_rating)
+#[1] 0.8663858
