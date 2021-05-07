d27bcb8155e035f8a11a87b9655577bf83fda74b
masayoshi sato <masayoshi418979@icloud.com>
2021-05-07 17:18:18 +0900

    changed a bit

--- movieLens.R
+++ movieLens.R
@@ -1,6 +1,3 @@
-#R codes
-
-################################################################################
 #used following libraries
 if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
 if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
@@ -161,7 +158,7 @@ edx %>% group_by(genres) %>%
   ggtitle("genres (n>20000)")
 
 ################################################################################
-#test and training data
+#making test and training data
 
 set.seed(1, sample.kind = "Rounding") 
 test_index <- createDataPartition(y = edx$rating, times = 1, p = 0.1, list = FALSE)
@@ -399,14 +396,19 @@ rmse_results %>% knitr::kable()
 #  |Reg Movie User Effects Model                    | 0.8644477|
 #  |Reg Movie User Genre Release Year Effects Model | 0.8639532|
 
+# final model "Reg Movie User Genre Release Year Effects Model" 
+#is proved to extract the least RMSE Value.
 ################################################################################
 #validation calculating lambda
 
+# add and cut columns in the process of training
 validation <- validation %>% mutate(release_year = as.numeric(str_sub(title,-5,-2)))
 validation <- validation %>% select(-timestamp)
 
+# define lambdas as a variable
 lambdas <- seq(0, 10, 0.25)
 
+#calculate rmse using "Reg Movie User Genre Release Year Effects Model" 
 val_rmse <- sapply(lambdas, function(l){
   
   mu <- mean(train_set$rating)
@@ -440,14 +442,17 @@ val_rmse <- sapply(lambdas, function(l){
     left_join(y_avg, by="release_year")%>%
     mutate(pred = mu + b_i + b_u + g + y) %>%
     pull(pred)
-    
-    val_pred <-replace(val_pred, is.na(val_pred),mu) #replace NA with mu
+   
+    #replace NA with mu(the average rating in the training)
+    val_pred <-replace(val_pred, is.na(val_pred),mu)
     
     return(RMSE(val_pred,validation$rating))
 })
 
+#plot lambdas and rmses
 qplot(lambdas,val_rmses)
 
+#find lambda and rmse which indicate the least value
 lambda <- lambdas[which.min(val_rmses)]
 lambda
 #[1] 5
@@ -456,80 +461,3 @@ min(val_rmses)
 #[1] 0.8646954
 
 ################################################################################
-# installing tinytex to knit pdf from rmd files
-install.packages(c('tinytex', 'rmarkdown'))
-tinytex::install_tinytex()
-
-# other methods
-#using lambda= 5
-lambda
-#[1] 5
-
-mu <- mean(train_set$rating) 
-
-reg_movie_avg <- train_set %>% 
-  group_by(movieId) %>%
-  summarize(b_i = sum(rating - mu)/(n()+lambda), n_i =n())
-
-reg_user_avg <- train_set %>% 
-  left_join(reg_user_avg, by="movieId") %>%
-  group_by(userId) %>%
-  summarize(b_u = sum(rating - b_i - mu)/(n()+lambda), n_u=n())
-
-g_avg <- train_set %>% 
-  left_join(reg_movie_avg, by="movieId") %>%
-  left_join(reg_user_avg, by="userId") %>%
-  group_by(genres) %>%
-  summarize(g = sum(rating - b_i -b_u - mu)/(n()+lambda), n_g=n())  
-
-y_avg <- train_set %>% 
-  left_join(reg_movie_avg, by="movieId") %>%
-  left_join(reg_user_avg, by="userId") %>%
-  left_join(g_avg, by="genres") %>%
-  group_by(release_year) %>%
-  summarize(y = sum(rating - b_i -b_u -g - mu)/(n()+lambda),n_y=n()) 
-
-predicted_ratings <-test_set %>% 
-  left_join(reg_movie_avg, by = "movieId") %>%
-  left_join(reg_user_avg, by = "userId") %>%
-  left_join(g_avg, by="genres") %>%
-  left_join(y_avg, by="release_year")%>%
-  mutate(pred = mu + b_i + b_u + g + y) %>%
-  pull(pred)
-
-reg_movie_user_genre_year_rmse <-RMSE(predicted_ratings, test_set$rating)
-reg_movie_user_genre_year_rmse
-#[1] 0.86365
-
-validation <- validation %>% mutate(release_year = as.numeric(str_sub(title,-5,-2)))
-validation <- validation %>% select(-timestamp)
-
-
-test_val <- validation %>% 
-  semi_join(train_set, by = "movieId") %>%
-  semi_join(train_set, by = "userId") %>%
-  semi_join(train_set, by= "genres") %>%
-  semi_join(train_set, by="release_year")
-
-valid_pred_rating <-validation %>%
-  left_join(b_i, by = "movieId") %>%
-  left_join(b_u, by = "userId") %>%
-  left_join(g, by="genres") %>%
-  left_join(y, by="release_year")%>%
-  mutate(pred = mu + b_i + b_u +g +y)%>%
-  pull(pred)
-
-dat <- validation[which(is.na(valid_pred_rating))]
-
-validation_rmse <- RMSE(validation$rating, valid_pred_rating)
-validation_rmse
-#[1] 0.8646954
-
-
-val_predicted_ratings <-validation %>% 
-  left_join(reg_movie_avg, by = "movieId") %>%
-  left_join(reg_user_avg, by = "userId") %>%
-  left_join(g_avg, by="genres") %>%
-  left_join(y_avg, by="release_year")%>%
-  mutate(pred = mu + b_i + b_u + g + y) %>%
-  pull(pred)
