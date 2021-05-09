f6cda9d82146980c4fe5c3e0bbb53322a04c42d0
masayoshi sato <masayoshi418979@icloud.com>
2021-05-10 00:25:50 +0900

    almost finished

--- movieLens.R
+++ movieLens.R
@@ -10,6 +10,8 @@ library(data.table)
 library(lubridate)
 
 ################################################################################
+#Exploratory Analysis#
+
 #Create edx set, validation set (final hold-out test set). 
 #This code is provided by the course. 
 
@@ -41,43 +43,19 @@ removed <- anti_join(temp, validation)
 edx <- rbind(edx, removed)
 
 rm(dl, ratings, movies, test_index, temp, movielens, removed)
-################################################################################
-#Find basic information about the dataset, edx and validation
 
 #check the numbers of rows and columns of these datasets.
 dim(edx)
-#[1] 9000055       6
 dim(validation)
-#[1] 999999      6
-
-#edx's columns(predictors)
 
 #the first rows of edx
 head(edx)
-# userId movieId rating timestamp                         title
-# 1:      1     122      5 838985046              Boomerang (1992)
-# 2:      1     185      5 838983525               Net, The (1995)
-# 3:      1     292      5 838983421               Outbreak (1995)
-# 4:      1     316      5 838983392               Stargate (1994)
-# 5:      1     329      5 838983392 Star Trek: Generations (1994)
-# 6:      1     355      5 838984474       Flintstones, The (1994)
-
-# genres
-# 1:                Comedy|Romance
-# 2:         Action|Crime|Thriller
-# 3:  Action|Drama|Sci-Fi|Thriller
-# 4:       Action|Adventure|Sci-Fi
-# 5: Action|Adventure|Drama|Sci-Fi
-# 6:       Children|Comedy|Fantasy
 
 ################################################################################
-#check each column by plotting 
-
-#"rating"
+#Rating#
 
 #find out its range
 unique(edx$rating)%>% sort() 
-# [1] 0.5 1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0
 
 #plot its distribution
 edx %>%
@@ -85,11 +63,11 @@ edx %>%
   geom_bar() + 
   ggtitle("Ratings")
 
-#"useId"
+################################################################################
+#Users#
 
 #find unique number of users
 n_distinct(edx$userId)
-#[1] 69878
 
 #plot histogram of userID
 edx %>%
@@ -99,7 +77,8 @@ edx %>%
   scale_x_log10() +
   ggtitle("Users")
 
-#"movieId"
+################################################################################
+#Movies#
 
 #find unique number of movies
 n_distinct(edx$movieId)
@@ -111,26 +90,28 @@ edx %>%
   geom_histogram(bins = 30, color = "black") + 
   scale_x_log10() + 
   ggtitle("Movies")
-#[1] 10677
 
-#"timestamp"
+################################################################################
+#Timestamp#
 
 #covert "timestamp" to POSIXct data
 edx$timestamp <- as.POSIXct(edx$timestamp, origin= "1970-01-01", tz="GMT")
 
-#and find months of ratings.It is named "date"
+#months of ratings column is added to the edx.It is named "date"
 edx <- edx %>% mutate(date = round_date(timestamp, unit = "month"))  
 
-#plot date and rating
+#plot date and rating, show regression line
 edx%>%
   group_by(date) %>%
   summarize(ratings =mean(rating)) %>% 
   ggplot(aes(x = date, y=ratings)) +geom_point() + geom_smooth(method = "lm")
   ggtitle("Months of rating")
-  
-#"title"
 
-#extract the release years from the title. It is named "release year"
+################################################################################    
+#Title#
+
+#extract the release years from the title, and its column is added to the edx
+#It is named "release year"
 edx <- edx %>% mutate(release_year = as.numeric(str_sub(title,-5,-2)))
 
 #plot release year and rating
@@ -140,17 +121,18 @@ edx %>% group_by(release_year) %>%
   geom_point()+geom_smooth(method="lm")+
   ggtitle("Release_year")
 
-#"genres"
+################################################################################
+#Genres
 
 #find the number of unique genres
 edx$genres %>% n_distinct()
 
-#top 10 genres in terms of rating
+#top10 high rated genres
 edx %>% group_by(genres)%>%
   summarize(ratings = mean(rating))%>%top_n(10)
 
 #plot genres and rating. 
-#using genres which have more than 20000 ratings.
+#showing genres which have more than 20000 ratings.
 edx %>% group_by(genres) %>%
   summarize(n = n(), avg = mean(rating)) %>%
   filter(n >= 20000) %>% 
@@ -161,6 +143,8 @@ edx %>% group_by(genres) %>%
   ggtitle("Genres (n>20000)")
 
 ################################################################################
+#Exploration Summary#
+
 #making test and training data 10% train_set, 90% test_set
 
 set.seed(1, sample.kind = "Rounding") 
@@ -180,20 +164,24 @@ test_set <- test_set %>%
 RMSE <- function(true_ratings, predicted_ratings){
   sqrt(mean((true_ratings - predicted_ratings)^2))
 }
-################################################################################
-#1 Naive model assuming the same rating for all movies.
 
+################################################################################
+#Model Building#
+#1 Baseline model, assuming the same rating for all movies#
+
+#produce rmse
 mu <- mean(train_set$rating)
 naive_rmse <- RMSE(test_set$rating, mu)
 naive_rmse
-#[1] 1.060054
 
-rmse_results <- tibble(method = "Rating Average", RMSE = naive_rmse)%>% as.data.frame()
-rmse_results
+#make a table
+rmse_results <- tibble(method = "1 Average rating", RMSE = naive_rmse)
+rmse_results %>% knitr::kable()
 
 ################################################################################
-#2 movie effects
+#2 Movie effects#
 
+#produce rmse
 mu <- mean(train_set$rating)
 movie_avg <- train_set %>%
   group_by(movieId)%>%
@@ -205,15 +193,17 @@ movie_effect_pred <- mu + test_set %>%
 
 movie_effect_rmse <- RMSE(test_set$rating, movie_effect_pred)
 movie_effect_rmse
-#[1] 0.9429615
 
+#make a table
 rmse_results <- bind_rows(rmse_results,
-                          tibble(method="Movie Effects Model",  
-                                     RMSE =movie_effect_rmse))%>% as.data.frame()
-rmse_results
-################################################################################
-#3 movie and user effects
+                          tibble(method="2 Movie Effects Model",  
+                                     RMSE =movie_effect_rmse))
+rmse_results %>% knitr::kable()
 
+################################################################################
+#3 movie and user effects#
+
+#produce rmse
 user_avg <- train_set %>%
   left_join(movie_avg, by= "movieId") %>%
   group_by(userId) %>%
@@ -225,42 +215,24 @@ movie_user_effect_pred <- test_set %>%
   mutate(pred = mu + b_i + b_u)%>%
   pull(pred)
 
+#make a table
 movie_user_effect_rmse <- RMSE(test_set$rating, movie_user_effect_pred)
 movie_user_effect_rmse
-#[1] 0.8646844
 
 rmse_results <- bind_rows(rmse_results,
-                          tibble(method="Movie User Effects Model",  
-                                 RMSE =movie_user_effect_rmse))%>% as.data.frame()
-rmse_results
+                          tibble(method="3 Movie User Effects Model",  
+                                 RMSE =movie_user_effect_rmse))
+rmse_results %>% knitr::kable()
 
 ################################################################################
-#4 genre effects
+#4 Genres and release year effects#
 
+#produce rmse, using two variables, genres and release year 
 g_avg <- train_set %>%
   left_join(movie_avg, by= "movieId")%>%
   left_join(user_avg, by="userId") %>%
   group_by(genres) %>%
   summarize(g = mean(rating- mu -b_i -b_u))
-
-movie_user_genre_effect_pred <- test_set %>%
-  left_join(movie_avg, by="movieId") %>%
-  left_join(user_avg, by="userId") %>%
-  left_join(g_avg, by="genres") %>%
-  mutate(pred = mu + b_i + b_u + g)%>%
-  pull(pred)
-
-movie_user_genre_effect_rmse <- RMSE(test_set$rating, movie_user_genre_effect_pred)
-movie_user_genre_effect_rmse
-#[1] 0.8643242
-
-rmse_results <- bind_rows(rmse_results,
-                          tibble(method="Movie User Genre Effects Model",  
-                                 RMSE =movie_user_genre_effect_rmse))%>% as.data.frame()
-rmse_results
-
-################################################################################
-#5 release year effects
 
 y_avg <- train_set %>%
   left_join(movie_avg, by= "movieId")%>%
@@ -279,19 +251,20 @@ movie_user_genre_year_effect_pred <- test_set %>%
 
 movie_user_genre_year_effect_rmse <- RMSE(test_set$rating, movie_user_genre_year_effect_pred)
 movie_user_genre_year_effect_rmse
-#[1] 0.8641262
 
+#make a table
 rmse_results <- bind_rows(rmse_results,
-                          tibble(method="Movie User Genre Release Year Effects Model",  
-                                 RMSE =movie_user_genre_year_effect_rmse))%>% as.data.frame()
-rmse_results
+                          tibble(method="4 Movie User Genre Release Year Effects Model",  
+                                 RMSE =movie_user_genre_year_effect_rmse))
+rmse_results %>% knitr::kable()
+
 ################################################################################
-#6 regularization (movie, user) 
+#5 regularization (movie, user) #
 
-#introducing lambda
-
+#denoting lambda as a variable
 lambdas <- seq(0, 10, 0.25)
 
+#calculate rmses which correspond to the value of lambdas  
 rmses <- sapply(lambdas, function(l){
   
   mu <- mean(train_set$rating)
@@ -315,27 +288,30 @@ rmses <- sapply(lambdas, function(l){
   return(RMSE(predicted_ratings, test_set$rating))
 })
 
+#plot lambda and rmses
 qplot(lambdas,rmses)
 
+#find lambda that minimizes the rmse.
 lambda <- lambdas[which.min(rmses)]
 lambda
-#[1] 5
 
+#show rmse
 reg_movie_user_rmse <- rmses
 reg_movie_user_rmse[5]
-#[1] 0.8644477
 
+#make a table
 rmse_results <- bind_rows(rmse_results,
-                          tibble(method="Reg Movie User Effects Model",  
-                                 RMSE =reg_movie_user_rmse[5]))%>% as.data.frame()
-rmse_results
+                          tibble(method="5 Reg Movie User Effects Model",  
+                                 RMSE =reg_movie_user_rmse[5]))
+rmse_results %>% knitr::kable()
+
 ###############################################################################
-#7 regularization  (movie, user,genres, release_year)
+#6 regularization  (movie, user,genres, release_year)#
 
-#calculating lambda
-
+#denoting lambda as a variable
 lambdas <- seq(0, 10, 0.25)
 
+#calculate rmses which correspond to the value of lambdas
 rmses <- sapply(lambdas, function(l){
   
   mu <- mean(train_set$rating)
@@ -373,39 +349,31 @@ rmses <- sapply(lambdas, function(l){
   return(RMSE(predicted_ratings, test_set$rating))
 })
 
+#plot lambda and rmses
 qplot(lambdas,rmses)
+
+#find the lambda minimize the RMSE
 lambda <- lambdas[which.min(rmses)]
 lambda
-#[1] 4.5
 
+#the RMSE
 min(rmses)
-#[1] 0.8636478
 
 #model summary
 rmse_results <- bind_rows(rmse_results,
-                          tibble(method="Reg Movie User Genre Release Year Effects Model",  
+                          tibble(method="6 Reg Movie User Genre Release Year Effects Model",  
                                  RMSE =rmses[4.5]))%>% as.data.frame()
 rmse_results
 rmse_results %>% knitr::kable()
 
-#  |method                                          |      RMSE|
-#  |:-----------------------------------------------|---------:|
-#  |Rating Average                                  | 1.0600537|
-#  |Movie Effects Model                             | 0.9429615|
-#  |Movie User Effects Model                        | 0.8646844|
-#  |Movie User Genre Effects Model                  | 0.8643242|
-#  |Movie User Genre Release Year Effects Model     | 0.8641262|
-#  |Reg Movie User Effects Model                    | 0.8644477|
-#  |Reg Movie User Genre Release Year Effects Model | 0.8639532|
-
 # final model "Reg Movie User Genre Release Year Effects Model" 
-#is proved to extract the least RMSE Value.
-################################################################################
-#validation calculating lambda
 
-# add and cut columns in the process of training
+################################################################################
+#Evaluation#
+
+#add a column  to the validation set
+#it is produced in the process of finding models
 validation <- validation %>% mutate(release_year = as.numeric(str_sub(title,-5,-2)))
-validation <- validation %>% select(-timestamp)
 
 # define lambdas as a variable
 lambdas <- seq(0, 10, 0.25)
@@ -457,9 +425,12 @@ qplot(lambdas,val_rmse)
 #find lambda and rmse which indicate the least value
 lambda <- lambdas[which.min(val_rmse)]
 lambda
-#[1] 5
 
-min(val_rmse)
-#[1] 0.8646978
+#Final RMSE
+Final_RMSE <-min(val_rmse)
+Final_RMSE
 
 ################################################################################
+
+
+  
